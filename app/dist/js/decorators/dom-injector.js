f5171519d74ad0cb886c918d827817ad26d3105f
caiquenerivan <caiquenerivansantos@gmail.com>
2023-02-17 00:19:31 -0300

    terceira aula do curso 3 finalizada

---
+++ app/dist/js/decorators/dom-injector.js
@@ -0,0 +1,13 @@
+export function domInjector(seletor) {
+    return function (target, propertyKey) {
+        let elemento = null;
+        const getter = function () {
+            if (!elemento) {
+                elemento = document.querySelector(seletor);
+                console.log(`Buscando elemento do DOM com o ${seletor} para injetar em ${propertyKey}`);
+            }
+            return elemento;
+        };
+        Object.defineProperty(target, propertyKey, { get: getter });
+    };
+}
